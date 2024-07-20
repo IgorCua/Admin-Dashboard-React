09c747664362a8ddc385dd1ac04f6962d60551dd
IhorC. <igorchua@gmail.com>
2024-07-20 17:56:26 -0500

    feat: App theme context added; feat: Topbar box, button added;

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { ColorModeContext, useMode } from "./styles/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from './scenes/global/Topbar';
+
 function App() {
-  return (
-    <div className="app"></div>
-  );
+    const [theme, colorMode] = useMode();
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+          <ThemeProvider>
+            <CssBaseline/>
+            <div className="app">
+              <main className="content"></main>
+            </div>;
+          </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,3 +1,21 @@
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../styles/theme";
+import { 
+    LightModeOutlinedIcon,
+    DarkModeOutlinedIcon,
+    NotificationsOutlinedIcon,
+    SettingsOutlinedIcon,
+    PersonOutlinedIcon,
+    SearchIcon,
+} from "@mui/icons-material";
+
 export const Topbar = () => {
-    return <div>Topbar</div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
+    return <Box display='flex' justifyContent='space-between' p={2}>
+        <IconButton sx={{display: 'flex'}}></IconButton>
+    </Box>
 }
