f7fdce4d767ca7a0d936fb42ab26d4222cca76ad
IhorC. <igorchua@gmail.com>
2024-07-20 19:43:17 -0500

    feat: Topbar imports added; feat: react-pro-sidebar installed; feat: App routes created;

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+import * as ProSidebar from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButtom, Typography, useTheme } from '@mui/material';
+import { Link } from "react-router-dom";
+import { tokens } from "../../styles/theme";
+import HomeModeOutlinedIcon from '@mui/icons-material/HomeModeOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import BarOutlinedIcon from '@mui/icons-material/BarOutlined';
+import PieChartOutlinedIcon from '@mui/icons-material/PieChartOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+
 export const Sidebar = () => {
+    
     return <div>Sidebar</div>
 }
