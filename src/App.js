a9920e95b34b1a48ddbc9eb1a82db0dfffecaabd
IhorC. <igorchua@gmail.com>
2024-07-20 19:15:54 -0500

    fix: theme didnt't change; feat: App dashboard route added;

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import { ColorModeContext, useMode } from "./styles/theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Topbar from './scenes/global/Topbar';
+import { Route, Routes } from "react-router";
+import { Dashboard } from './scenes/dashboard/Index';
+import { Topbar } from './scenes/global/Topbar';
+// import { Sidebar } from './scenes/global/Sidebar';
+// import { Team } from './scenes/team';
+// import { Invoices } from './scenes/invoices';
+// import { Contacts } from './scenes/contacts';
+// import { Bar } from './scenes/bar';
+// import { Form } from './scenes/form';
+// import { Line } from './scenes/line';
+// import { Pie } from './scenes/Pie';
+// import { FAQ } from './scenes/faq';
+// import { Geography } from './scenes/geography';
 
 function App() {
     const [theme, colorMode] = useMode();
 
     return (
         <ColorModeContext.Provider value={colorMode}>
-          <ThemeProvider>
+          <ThemeProvider theme={theme}>
             <CssBaseline/>
             <div className="app">
-              <main className="content"></main>
+              <main className="content">
+                <Topbar/>
+                <Routes>
+                  <Route path='/' element={<Dashboard/>}/>
+                </Routes>
+              </main>
             </div>;
           </ThemeProvider>
         </ColorModeContext.Provider>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter basename='/'>
+          <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
 
