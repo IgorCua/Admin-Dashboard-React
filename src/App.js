e86eb0fcc53b30942251ec0eb4db8367267db126
IhorC. <igorchua@gmail.com>
2024-07-24 09:43:56 -0500

    feat: Bar page added;

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import { Sidebar } from './scenes/global/Sidebar';
 import { Team } from './scenes/team/Index.jsx';
 import { Invoices } from './scenes/invoices/Index.jsx';
 import { Contacts } from './scenes/contacts/index.jsx';
-// import { Bar } from './scenes/bar';
 import { Form } from './scenes/form/Index.js';
-// import { Line } from './scenes/line';
-// import { Pie } from './scenes/Pie';
-// import { FAQ } from './scenes/faq';
-// import { Geography } from './scenes/geography';
 import { Calendar } from './scenes/calendar/Index.jsx';
+import { FAQ } from './scenes/faq/Index.jsx';
+import { Bar } from './scenes/bar/Index.jsx';
+import { Pie } from './scenes/pie/Index.jsx';
+// import { Line } from './scenes/line';
+// import { Geography } from './scenes/geography';
 
 function App() {
     const [theme, colorMode] = useMode();
@@ -32,12 +32,12 @@ function App() {
                   <Route path='/contacts' element={<Contacts/>}/>
                   <Route path='/invoices' element={<Invoices/>}/>
                   <Route path='/form' element={<Form/>}/>
-                  {/* <Route path='/bar' element={<Bar/>}/> */}
-                  {/* <Route path='/pie' element={<Pie/>}/> */}
-                  {/* <Route path='/line' element={<Line/>}/> */}
-                  {/* <Route path='/faq' element={<Faq/>}/> */}
-                  {/* <Route path='/geography' element={<Geography/>}/> */}
                   <Route path='/calendar' element={<Calendar/>}/>
+                  <Route path='/faq' element={<FAQ/>}/>
+                  <Route path='/bar' element={<Bar/>}/>
+                  <Route path='/pie' element={<Pie/>}/>
+                  {/* <Route path='/line' element={<Line/>}/> */}
+                  {/* <Route path='/geography' element={<Geography/>}/> */}
                 </Routes>
               </main>
             </div>
